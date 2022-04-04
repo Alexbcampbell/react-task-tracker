f3c34d9215a33eb56ec85a78f5693583b3513066
Alex Campbell <alexbe.campbell@gmail.com>
2022-04-04 16:08:39 -0500

    ternary operator to show no task message, if no tasks

--- src/App.js
+++ src/App.js
@@ -30,13 +30,15 @@ function App() {
 
 //delete task
 const deleteTask = (id) => {
-  console.log('delete', id)
+  setTasks(tasks.filter((task) => task.id !== id))
 }
 
   return (
     <div className='container'>
      <Header />
-     <Tasks tasks={tasks} onDelete={deleteTask} />
+     {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : (
+       'No Tasks To Show'
+     )}
     </div>
   );
 }
