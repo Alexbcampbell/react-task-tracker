8b43469c5fa61d5c1b131ef6533555940cfaf351
Alex Campbell <alexbe.campbell@gmail.com>
2022-04-04 15:51:48 -0500

    created Task js, added tasks array to app js, installed react icons

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import './App.css';
 import Header from './components/Header'
 import Tasks from './components/Tasks';
 
+import { useState } from "react"
+
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'Appointment',
+            day: 'April 5',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Meeting',
+            day: 'April 12',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Shopping',
+            day: 'April 11',
+            reminder: true,
+        }
+    ]
+)
   return (
     <div className='container'>
      <Header />
-     <Tasks />
+     <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,28 +1,11 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Appointment',
-        day: 'April 5',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting',
-        day: 'April 12',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Shopping',
-        day: 'April 11',
-        reminder: true,
-    }
-]
+import Task from "./Task"
 
-const Tasks = () => {
+const Tasks = ({tasks}) => {
   return (
     <>
-    {tasks.map((task) => (<h3>{task.text}</h3>))}
+    {tasks.map((task) => (
+        <Task key={task.id} task={task} />
+    ))}
     </>
   )
 }
