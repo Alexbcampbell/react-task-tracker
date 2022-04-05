ff9775f9918800ad2677da6973786b08dce8d234
Alex Campbell <alexbe.campbell@gmail.com>
2022-04-05 12:03:04 -0500

    Add on change and target event values to inputs for Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header'
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 import { useState } from "react"
 
@@ -41,6 +42,7 @@ setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder
   return (
     <div className='container'>
      <Header />
+     <AddTask />
      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : (
        'No Tasks To Show'
      )}
