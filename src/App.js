9babc393de847bb0c09ede194ba284aadf7af965
Alex Campbell <alexbe.campbell@gmail.com>
2022-04-05 14:09:12 -0500

    addTask function created on App js, declares new task and sets ID

--- src/App.js
+++ src/App.js
@@ -31,7 +31,9 @@ function App() {
 
 //add task
 const addTask = (task) => {
-  console.log(task);
+  const id = Math.floor(Math.random()*10000 +1)
+  const newTask = {id, ...task  }
+  setTasks([...tasks, newTask])
 }
 
 //delete task
