54b7b6f2cda81a17efbf843a962b0bd057527bc6
Alex Campbell <alexbe.campbell@gmail.com>
2022-04-05 14:24:31 -0500

    ternary's added for switching close or add and button colors

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder
 
   return (
     <div className='container'>
-     <Header onAdd={() => setShowAddTask(!showAddTask)} />
+     <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
      {showAddTask && <AddTask onAdd={addTask} />}
      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : (
        'No Tasks To Show'
